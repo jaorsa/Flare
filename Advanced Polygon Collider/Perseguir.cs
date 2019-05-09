2e84b9ab167f9035cfdcdf3f2d93eb28e3748f5e
jaorsa <38358814+jaorsa@users.noreply.github.com>
2019-05-09 13:16:44 -0500

    Add files via upload

---
+++ Advanced Polygon Collider/Perseguir.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perseguir : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 distancia;
+    public float retardo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 posicionDeseada = player.position + distancia;
+        Vector3 posicionRetardo = Vector3.Lerp(transform.position, posicionDeseada, retardo);
+        transform.position = posicionRetardo;
+
+        transform.LookAt(player);
+    }
+}
